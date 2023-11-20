8780db24ff037d5e2120581adb55638a0ec98109
i komang ary antara <ikomangarynantara382@gmail.com>
2023-11-20 20:33:51 +0800

    add some element into karyawan menu

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,22 @@
+<div class="w-[400px] h-screen bg-[#368D5B]">
+    <div class="flex flex-col items-center">
+        <a class="w-max mt-20" href="#">
+            <img src="{{ asset('images/logo.png') }}" alt="" class="w-20 h-20">
+        </a>
+        <a class="mt-10 mb-4" href="#">
+            <div class="p-2 shadow-lg bg-[#ffffff] w-[200px] rounded-3xl hover:bg-orange-300 text-center align-center">
+				<i class="me-2 bi bi-house-fill"></i>
+                <span class="text-sm text-gray-900 font-bold text-black">Dashboard</span>
+            </div>
+        </a>     
+
+		<hr class="my-2 border-t-1 w-[200px]"></hr>
+
+		<a class="mt-4" href="#">
+            <div class="p-2 shadow-lg bg-[#ffffff] w-[200px] rounded-3xl hover:bg-orange-300 text-center align-center">
+				<i class="me-2 bi bi-people-fill"></i>
+                <span class="text-sm text-gray-900 font-bold text-black">Karyawan</span>
+            </div>
+        </a>     
+    </div>
+</div>
--- resources/views/pages/menu-karyawan.blade.php
+++ resources/views/pages/menu-karyawan.blade.php
@@ -1,64 +1,107 @@
 @extends('layouts.default')
 
 @section('content')
-    <!-- Component Start -->
-    <x-dashboard>
-        logo
-    </x-dashboard>
-    <!-- Component End  -->
+    <div class="flex">
+        <!-- Component Sidebar Start -->
+        <x-sidebar>
+            logo
+        </x-sidebar>
+        <!-- Component End  -->
 
-    <!-- body -->
-    <div class="w-full">
-        <div class="h-36">
-            <div class="py-11 px-12 text-[#368D5B] font-bold">
-                KARYAWAN
+        <!-- body -->
+        <div class="m-16 w-full">
+            <div class="h-36">
+                <div class="text-[#368D5B] font-bold text-3xl">
+                    KARYAWAN
+                </div>
+                <div class="flex justify-between items-center mt-10">
+                    <div class="bg-[#368D5B] w-max p-1 rounded-full h-max">
+                        <i class="bi mx-2 bi-search text-white"></i>
+                        <input type="text" class="w-auto bg-[#368D5B] outline-none text-white mx-2 p-1 placeholder-white"
+                            placeholder="Cari nama staff...">
+                    </div>
+
+                    <button class="bg-[#368D5B] text-xl rounded-full text-white p-2">
+                        <i class="text-2xl bi bi-plus m-1"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="w-full h-auto mt-8">
+                <div class="overflow-x-scroll overflow-y-hidden">
+                    <table class="w-full border-collapse">
+                        <thead class="text-[#368D5B] border-b font-bold align-left p-32">
+                            <tr class="">
+                                <th class="py-2" align="left">NAMA</th>
+                                <th class="py-2" align="left">NAMA</th>
+                                <th class="py-2" align="left">NAMA</th>
+                                <th class="py-2" align="left">NAMA</th>
+                                <th class="py-2" align="left">NAMA</th>
+                                <th class="py-2" align="left">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="py-2 px-1 border-1 bg-green-50">testt</td>
+                                <td class="py-2 px-1 border-1 bg-green-50">testt</td>
+                                <td class="py-2 px-1 border-1 bg-green-50">testt</td>
+                                <td class="py-2 px-1 border-1 bg-green-50">testt</td>
+                                <td class="py-2 px-1 border-1 bg-green-50">testt</td>
+                                <td class="py-2 px-1 border-1 bg-green-50 w-[300px]">
+                                    <div class="flex">
+                                        <div class="mx-1 p-2">
+                                            <bi class="text-xl bi-eye-fill"></bi>
+                                        </div>
+
+                                        <div class="mx-1 p-2">
+                                            <bi class="text-xl bi-trash-fill"></bi>
+                                        </div>
+
+                                        <div class="mx-1 p-2">
+                                            <bi class="text-xl bi-pencil-fill"></bi>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="py-2 px-1 border-1">testt</td>
+                                <td class="py-2 px-1 border-1">testt</td>
+                                <td class="py-2 px-1 border-1">testt</td>
+                                <td class="py-2 px-1 border-1">testt</td>
+                                <td class="py-2 px-1 border-1">testt</td>
+                                <td class="py-2 px-1 border-1"> <div class="flex">
+                                    <div class="mx-1 p-2">
+                                        <bi class="text-xl bi-eye-fill"></bi>
+                                    </div>
+
+                                    <div class="mx-1 p-2">
+                                        <bi class="text-xl bi-trash-fill"></bi>
+                                    </div>
+
+                                    <div class="mx-1 p-2">
+                                        <bi class="text-xl bi-pencil-fill"></bi>
+                                    </div>
+                                </div></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="flex justify-end mt-2">
+                    <div class="rounded-full w-max h-[40px] bg-gray-100 flex items-center">
+                        <button class="hover:bg-[#368D5B] hover:text-white text-[#368D5B] m-2 rounded-full p-1 px-2">
+                            <i class="text-xl bi bi-chevron-left"></i>
+                        </button>
+                        <button class="hover:bg-[#368D5B] hover:text-white text-[#368D5B] my-2 rounded-full p-1 px-2">
+                            1
+                        </button>
+                        <button class="hover:bg-[#368D5B] hover:text-white text-[#368D5B] my-2 rounded-full p-1 px-2">
+                            2
+                        </button>
+                        <button class="hover:bg-[#368D5B] hover:text-white text-[#368D5B] m-2 rounded-full p-1 px-2">
+                            <i class="text-xl bi bi-chevron-right"></i>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
-        <table class="w-full ml-11 border-collapse">
-            <thead class="text-green-500 border-b">
-                <tr>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                </tr>
-                <tr>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                </tr>
-                <tr>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                </tr>
-                <tr>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                    <td>testt</td>
-                </tr>
-            </tbody>
-        </table>
     </div>
 @endsection
